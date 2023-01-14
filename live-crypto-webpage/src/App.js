320efe44b87c748f62ca021a786d4f7dcfa13347
voidrlm <varjun609@gmail.com>
2023-01-15 04:03:30 +0530

    Implementation to fetch and store crypto data

--- live-crypto-webpage/src/App.js
+++ live-crypto-webpage/src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
-
+import React, { useState, useEffect } from "react";
+import { getCoinData } from "./services/api";
 function App() {
+  const [listOfCoins, setCoinData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await getCoinData();
+      setCoinData(data);
+    };
+  }, []);
+
   return (
     <div>
       <h1>Live Crypto</h1>
